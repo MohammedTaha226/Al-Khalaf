fdf64107fa67bfd32de5c1be1ec1660f20a05973
Mohammed Taha <mtelrazky@gmail.com>
2023-10-13 21:53:52 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,70 @@
+$(function(){
+
+	$.scrollIt({topOffset: 0});
+
+
+
+	$('.the-slider-inner').bxSlider({
+		useCSS: false,
+		auto: true,
+		controls: true,
+		pager: true,
+		autoHover: true,
+		responsive: true,
+		nextSelector: '#slider-next',
+		prevSelector: '#slider-prev',
+		nextText: '<i class="fa fa-angle-right"></i>',
+		prevText: '<i class="fa fa-angle-left"></i>'
+	});
+	
+		$(".header .open-menu").click(function() {
+			
+			$("header .navBar ul").slideToggle();
+			
+		});
+	
+	
+	  var owlServices = $('#owlServices');
+	 
+	  owlServices.owlCarousel({
+	      
+	      items : 3, //10 items above 1000px browser width
+	      itemsDesktop : [1200,3], //5 items between 1000px and 901px
+	      itemsDesktopSmall : [979,2], // betweem 900px and 601px
+	      itemsTablet: [768,2], //2 items between 600 and 0
+	      itemsMobile : [479,1],// itemsMobile disabled - inherit from itemsTablet option
+	      slideSpeed : 500,
+	      paginationSpeed : 400,
+	      pagination:false,
+	      navigation:true,
+	      autoPlay:true,
+	      navigationText:["<i class='fa fa-angle-left fa-2x'></i>","<i class='fa fa-angle-right fa-2x'></i>"]
+	  });
+	
+	  var staffOwl = $('#staffOwl');
+	 
+	  staffOwl.owlCarousel({
+	      
+	      items : 4, //10 items above 1000px browser width
+	      itemsDesktop : [1200,4], //5 items between 1000px and 901px
+	      itemsDesktopSmall : [979,3], // betweem 900px and 601px
+	      itemsTablet: [768,2], //2 items between 600 and 0
+	      itemsMobile : [479,1],// itemsMobile disabled - inherit from itemsTablet option
+	      slideSpeed : 500,
+	      paginationSpeed : 400,
+	      pagination:false,
+	      navigation:false,
+	      autoPlay:true,
+	      navigationText:["<i class='fa fa-angle-left fa-2x'></i>","<i class='fa fa-angle-right fa-2x'></i>"]
+	  });
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
